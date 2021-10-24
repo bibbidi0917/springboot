9ea55db8961ef09510b83b6a721fb1bd495e6a21
bibbidi0917 <bibbidi0917@naver.com>
2021-10-24 17:54:07 +0900

    4장 완성

--- src/main/java/com/koscom/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/koscom/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,14 @@
 package com.koscom.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //JpaRepository 상속 받으면 CRUD가 자동 구현됨
 public interface PostsRepository extends JpaRepository<Posts, Long> {//대상 entity, pk type
 
+    //SQL이 아니라 JPQL (JPA)
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/koscom/springboot/service/PostsService.java
+++ src/main/java/com/koscom/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.koscom.springboot.service;
 
 import com.koscom.springboot.domain.posts.Posts;
 import com.koscom.springboot.domain.posts.PostsRepository;
+import com.koscom.springboot.web.dto.posts.PostsListResponseDto;
 import com.koscom.springboot.web.dto.posts.PostsResponseDto;
 import com.koscom.springboot.web.dto.posts.PostsSaveRequestDto;
 import com.koscom.springboot.web.dto.posts.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //final로 선언된 필드들은 생성자 항목으로 자동 포함해서 생성자 생성
 @Service //spring bean 등록 & service 클래스 선언
@@ -17,14 +21,14 @@ public class PostsService {
 
     //등록
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         Posts posts = postsRepository.save(requestDto.toEntity());
         return posts.getId();
     }
 
     //수정
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto dto){
+    public Long update(Long id, PostsUpdateRequestDto dto) {
         // DB에서 가져온 값을 JPA 내부에서 캐시(1차 캐시)
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당사용자는 없습니다. id = " + id));
@@ -36,9 +40,24 @@ public class PostsService {
     }
 
     //조회 - myBatis 혹은 querydsl
-    public PostsResponseDto findById(Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당사용자는 없습니다. id = " + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream() //List<Posts>로 오면 그걸 스트림으로 바꾸고
+                .map(PostsListResponseDto::new) //post 하나하나를 PostsListResponseDto 로 바꿔서
+                .collect(Collectors.toList()); //list로 만들겠다
+    }
+
+    @Transactional //row lock을 잡아서 하나만
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당사용자는 없습니다. id = " + id));
+        postsRepository.delete(posts); // (1)
+    }
+
 }
